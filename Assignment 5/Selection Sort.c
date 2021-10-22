f810c153c297db04db114d98833225584d5dd96e
abhijit812 <91731000+abhijit812@users.noreply.github.com>
2021-10-22 15:53:16 +0530

    Add files via upload

--- Assignment 5/Selection Sort.c
+++ Assignment 5/Selection Sort.c
@@ -1,83 +1,37 @@
 #include <stdio.h>
- 
-void mergeSort(int [], int, int, int);
-void partition(int [],int, int);
- 
 int main()
 {
-    int list[50];
-    int i, size;
- 
-    printf("Enter total number of elements:");
-    scanf("%d", &size);
-    printf("Enter the elements:\n");
-    for(i = 0; i < size; i++)
+  int array[100], n, c, d, position, t;
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+
+  for (c = 0; c < (n - 1); c++) // finding minimum element (n-1) times
+  {
+    position = c;
+
+    for (d = c + 1; d < n; d++)
     {
-         scanf("%d", &list[i]);
+      if (array[position] > array[d])
+        position = d;
     }
-    partition(list, 0, size - 1);
-    printf("After merge sort:\n");
-    for(i = 0;i < size; i++)
+    if (position != c)
     {
-         printf("%d   ",list[i]);
+      t = array[c];
+      array[c] = array[position];
+      array[position] = t;
     }
- 
-   return 0;
-}
- 
-void partition(int list[],int low,int high)
-{
-    int mid;
- 
-    if(low < high)
-    {
-        mid = (low + high) / 2;
-        partition(list, low, mid);
-        partition(list, mid + 1, high);
-        mergeSort(list, low, mid, high);
-    }
-}
- 
-void mergeSort(int list[],int low,int mid,int high)
-{
-    int i, mi, k, lo, temp[50];
- 
-    lo = low;
-    i = low;
-    mi = mid + 1;
-    while ((lo <= mid) && (mi <= high))
-    {
-        if (list[lo] <= list[mi])
-        {
-            temp[i] = list[lo];
-            lo++;
-        }
-        else
-        {
-            temp[i] = list[mi];
-            mi++;
-        }
-        i++;
-    }
-    if (lo > mid)
-    {
-        for (k = mi; k <= high; k++)
-        {
-            temp[i] = list[k];
-            i++;
-        }
-    }
-    else
-    {
-        for (k = lo; k <= mid; k++)
-        {
-             temp[i] = list[k];
-             i++;
-        }
-    }
- 
-    for (k = low; k <= high; k++)
-    {
-        list[k] = temp[k];
-    }
+  }
+
+  printf("Sorted list in ascending order:\n");
+
+  for (c = 0; c < n; c++)
+    printf("%d ", array[c]);
+
+  return 0;
 }
